69159148f00024952d671412cc2b44b25dfd20fd
unknown <patxiarana05@gmail.com>
2024-07-06 11:59:24 -0300

    creando las vistas de login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,17 +1,23 @@
 <?php 
 
 
-namespace Controllers ; 
+namespace Controllers ;
 
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo "Desde login" ; 
+    public static function login(Router $router) {
 
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         }
+
+        //Render a la vista 
+        $router->render('auth/login', [
+            'titulo' => "iniciar sesión"
+
+        ]) ; 
     }
 
     public static function logout() {
@@ -20,13 +26,19 @@ class LoginController {
     }
 
 
-    public static function crear() {
-        echo "Desde crear" ; 
+    public static function crear(Router $router) { 
 
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         }
+
+        
+        //Render a la vista 
+        $router->render('auth/crear', [
+            'titulo' => "Crear cuenta"
+            
+        ]) ;
     }
 
     public static function olvide() {
