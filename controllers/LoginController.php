086ad3bffdce4ad4e7b7267703ebe885a1dcd694
unknown <patxiarana05@gmail.com>
2024-07-08 12:17:50 -0300

    creando la vista para confirmar

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -72,7 +72,9 @@ class LoginController {
       ]) ; 
     }
 
-    public static function confirmar() {
-        echo "Desde confirmar" ; 
+    public static function confirmar(Router $router) {
+        $router->render('auth/confirmar', [
+            "titulo" => 'Confirma tu cuenta UpTask'
+        ]) ; 
     }
 }
