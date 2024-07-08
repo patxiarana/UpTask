c0b8fb403826528dc53d314d8e4f75e503b9428e
unknown <patxiarana05@gmail.com>
2024-07-08 11:15:58 -0300

    creando la pagina de crear cuenta

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,57 @@
-<h1>Crea tu Cuenta</h1>
+<div class="contenedor crear">
+<?php include_once __DIR__ . '/../templates/nombre-sitio.php' ; ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
+
+
+
+        <form action="/" class="formulario" method="POST">
+            <div class="campo">
+              <label for="nombre">Nombre</label>
+              <input 
+              type="text"
+              id="nombre"
+              placeholder="Tu Nombre.."
+              name="nombre"
+               />
+            </div>
+
+
+        <form action="/" class="formulario" method="POST">
+            <div class="campo">
+              <label for="email">Email</label>
+              <input 
+              type="email"
+              id="email"
+              placeholder="Tu Email.."
+              name="email"
+
+              />
+            </div>
+            <div class="campo">
+              <label for="password">Password</label>
+              <input 
+              type="password"
+              id="password"
+              placeholder="Tu Password.."
+              name="password"
+             />
+            </div>
+            <div class="campo">
+              <label for="password2">Repetir Password</label>
+              <input 
+              type="password"
+              id="password2"
+              placeholder="Repite tu password.."
+              name="password2"
+             />
+            </div>
+            <input type="submit" class="boton" value="iniciar sesión">
+        </form>
+        <div class="acciones">
+            <a href="/">¿Ya tienes cuenta? Iniciar Sesión.</a>
+            <a href="/olvide">¿Olvidaste tu Password?</a>
+        </div>
+    </div> <!--contenedor sm-->
+</div>
