a123a005a0435ebbde045734334907cbdec458aa
unknown <patxiarana05@gmail.com>
2024-07-15 11:50:25 -0300

    creando la vista de dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -6,6 +6,8 @@ use MVC\Router;
 
 class DashboardController {
     public static function index(Router $router) {
+        session_start(); 
+
         $router->render('dashboard/index', []);
     }
 }
