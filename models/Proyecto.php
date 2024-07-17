59929b19e5e481603a0ccf175009e00d498e03ed
unknown <patxiarana05@gmail.com>
2024-07-17 10:07:05 -0300

    creando el modelo de proyecto

---
+++ models/Proyecto.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Proyecto;
+
+use Model\ActiveRecord;
+
+class Proyecto extends ActiveRecord {
+    protected static $tabla = 'proyectos';
+    protected static $columnasDB = ['id', 'proyecto', 'url', 'propietarioid'];
+
+    public $id;
+    public $proyecto;
+    public $url;
+    public $propietarioid;
+
+
+    public function __construct($args = []) 
+    {
+      $this->id = $args['id'] ?? null;
+      $this->proyecto = $args['proyecto']?? '';
+      $this->url = $args['url']?? '';
+      $this->propietarioid = $args['propietarioid']?? '';
+    }
+}
