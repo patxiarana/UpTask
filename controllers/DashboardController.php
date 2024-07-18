2e3e4fff7390982e5a826ef8412044f489f004ee
unknown <patxiarana05@gmail.com>
2024-07-18 09:17:07 -0300

    validacion para crear nuevos proyectos

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers ;
 
+use Model\Proyecto;
 use MVC\Router;
 
 class DashboardController {
@@ -9,7 +10,7 @@ class DashboardController {
         session_start(); 
 
         isAuth(); 
-        
+  
         $router->render('dashboard/index', [
             'titulo' => "Proyectos" , 
         ]);
@@ -18,8 +19,19 @@ class DashboardController {
     public static function crear_proyecto(Router $router) {
         session_start(); 
         isAuth();
-
         $alertas = [] ; 
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+             $proyecto = new Proyecto($_POST);
+             
+          //Validacion 
+          $alertas = $proyecto->validarProyecto();
+            
+          if(empty($alertas)) {
+            //Guardar el proyecto
+             
+          }
+            }      
+      
         $router->render('dashboard/crear-proyecto', [
             'alertas' => $alertas, 
             'titulo' => "Crear Proyecto" , 
--- models/Proyecto.php
+++ models/Proyecto.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace Proyecto;
+namespace Model;
 
 use Model\ActiveRecord;
 
@@ -20,5 +20,14 @@ class Proyecto extends ActiveRecord {
       $this->proyecto = $args['proyecto']?? '';
       $this->url = $args['url']?? '';
       $this->propietarioid = $args['propietarioid']?? '';
+    }
+
+    public function validarProyecto() {
+        if(!$this->proyecto) {
+               self::$alertas['error'][] = 'El nombre del proyecto es obligatorio';  
+            
+        }
+
+        return self::$alertas;
     }
 }
