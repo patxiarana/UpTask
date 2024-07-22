14c14156fa11223ee40dff307d7b272bcdf786d2
unknown <patxiarana05@gmail.com>
2024-07-22 09:14:54 -0300

    añadiendo javascript para mostrar el form

---
+++ src/js/tareas.js
@@ -0,0 +1,12 @@
+ 
+
+(function() {
+    //Boton para mostrar el Modal de agregar tarea 
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario)
+
+
+    function mostrarFormulario() {
+        console.log('mostrando form')
+    }
+})();
--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -13,3 +13,11 @@
 </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php' ; ?>
+
+
+<?php 
+$script = '
+<script src="build/js/tareas.js"></script>
+'
+
+?>
