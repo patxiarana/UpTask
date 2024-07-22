2480813d240777fa8feafc6439acea7bd5f8fde2
unknown <patxiarana05@gmail.com>
2024-07-22 09:34:36 -0300

    creando modal para añadir tarea

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -7,6 +7,29 @@
 
 
     function mostrarFormulario() {
-        console.log('mostrando form')
+       const modal = document.createElement('DIV');
+       modal.classList.add('modal');
+       modal.innerHTML = `
+          <form class="formulario nueva-tarea">
+          <legend>Añade una Nueva Tarea</legend>
+          <div class="campo">
+          <label>Tarea</label>
+          <input 
+            type="text"
+            id="tarea"
+            name="tarea"
+            placeholder="Ingresa una tarea"
+            />
+          </div>
+          <div class="opciones">
+          <input type="submit" 
+          class="submit-nueva-tarea"
+          value="Añadir tarea"/>
+          <button type="button" class="cerrar-modal">Cancelar</button>
+          </div>
+          </form>
+          `
+       
+    document.querySelector('body').appendChild(modal); 
     }
 })();
